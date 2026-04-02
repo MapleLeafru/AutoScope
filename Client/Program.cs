c85c5a79b878bc3c02cb5426eb38c2df32dd9329
MapleLeafru <seregache20041104@mail.ru>
2026-04-03 04:13:23 +1000

    Фиксируем ключевую точку в develop

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,10 @@ using System.Xml.Linq;
 // dotnet restore
 // python -m compileall AutoScope\Utils - компилирует файлы питона, после того как скачал на новую машину
 
+//import sys
+//!{sys.executable} -m pip install -r "../requirements.txt"
+//!{sys.executable} -m pip freeze > "../requirements.lock.txt"
+
 // На проде удалить ненужные пакеты: pip, setuptools wheel (если ты не будем ставить пакеты на проде) для того что бы уменьшить размер приложения, подробнее у gpt
 
 Console.WriteLine("C# Клиент запущен");
